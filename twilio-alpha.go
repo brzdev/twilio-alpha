3eea70563e7ed61cf1d0c4954542e4ce08041ad1
Aaron Brzowski <aaron.brzowski@gmail.com>
2017-02-27 17:50:59 -0600

    initial commit for a twilio service that can answer Wolfram Alpha Queries

---
+++ twilio-alpha.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"io/ioutil"
+	"net/url"
+	"bytes"
+
+)
+
+import (
+   "google.golang.org/appengine"
+	 "google.golang.org/appengine/urlfetch"
+
+)
+// [START import]
+import (
+	"twiml"
+	"twirest"
+)
+
+// [END import]
+
+func main() {
+	http.HandleFunc("/call/receive", receiveCallHandler)
+	http.HandleFunc("/sms/send", sendSMSHandler)
+	http.HandleFunc("/sms/receive", receiveSMSHandler)
+
+	appengine.Main()
+}
+
+var (
+	twilioClient = twirest.NewClient("AC098505c02bb36686294206913ff0dd08", "00d93aafdeaad239190b343f66762147")
+	twilioNumber = "3098073024"
+)
+
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("%s environment variable not set.", k)
+	}
+	return v
+}
+
+func receiveCallHandler(w http.ResponseWriter, r *http.Request) {
+	resp := twiml.NewResponse()
+	resp.Action(twiml.Say{Text: "Hello from App Engine!"})
+	resp.Send(w)
+}
+
+func sendSMSHandler(w http.ResponseWriter, r *http.Request) {
+	to := r.FormValue("to")
+	if to == "" {
+		http.Error(w, "Missing 'to' parameter.", http.StatusBadRequest)
+		return
+	}
+
+	msg := twirest.SendMessage{
+		Text: "Hello from App Engine!",
+		From: twilioNumber,
+		To:   to,
+	}
+
+	resp, err := twilioClient.Request(msg)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not send SMS: %v", err), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "SMS sent successfully. Response:\n%#v", resp.Message)
+}
+
+
+func receiveSMSHandler(w http.ResponseWriter, r *http.Request) {
+	sender := r.FormValue("From")
+	body := r.FormValue("Body")
+
+	var Url *url.URL
+			Url, _ = url.Parse("http://api.wolframalpha.com/v2/result?")
+
+			parameters := url.Values{}
+			parameters.Add("appid", "A72QEU-YEQT65L37E")
+			parameters.Add("i", string(body))
+			Url.RawQuery = parameters.Encode()
+
+			ctx := appengine.NewContext(r)
+			client := urlfetch.Client(ctx)
+	WAresp, _ := client.Get(Url.String())
+
+	WAbody, _ := ioutil.ReadAll(WAresp.Body)
+
+	WAfinal := (string(WAbody))
+
+	var buffer bytes.Buffer
+	buffer.WriteString(WAfinal)
+	buffer.WriteString(" - W|A")
+
+
+	resp := twiml.NewResponse()
+	resp.Action(twiml.Message{
+		Body: fmt.Sprintf(buffer.String()),
+		From: twilioNumber,
+		To:   sender,
+	})
+	resp.Send(w)
+}
